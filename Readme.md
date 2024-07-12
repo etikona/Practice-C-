ceaa765a39e277efe2ade565cf33d28dcc544ef9
eti kona <etikonapal@gmail.com>
2024-07-12 23:52:38 +0600

    Readme file added and code are comment off

---
+++ Readme.md
@@ -0,0 +1,1 @@
+## All the Practice files are here.
--- jumps.cpp
+++ jumps.cpp
@@ -2,26 +2,26 @@
 using namespace std;
 int main() {
     //* Print all the numbers from 0 to 100 but only skip these numbers which are dividable with 3.
-    // for(int i=0; i <=100; i++){
-    //     if(i%3==0){
-    //         continue;
-    //     }
-    //     cout <<i << endl;
-    // }
+    for(int i=0; i <=100; i++){
+        if(i%3==0){
+            continue;
+        }
+        cout <<i << endl;
+    }
     // * Check the numbers are prime or not.
-    // int n;
-    // cout <<"give the value of n = ";
-    // cin >> n;
-    // int i;
-    // for(i=2; i <=n; i++){
-    //     if(n%i==0){
-    //         cout << "It is a non prime number" << endl;
-    //         break;
-    //     }
-    // }
-    // if(n==i){
-    //     cout << n << endl << "It is a Prime." << endl;
-    // }
+    int n;
+    cout <<"give the value of n = ";
+    cin >> n;
+    int i;
+    for(i=2; i <=n; i++){
+        if(n%i==0){
+            cout << "It is a non prime number" << endl;
+            break;
+        }
+    }
+    if(n==i){
+        cout << n << endl << "It is a Prime." << endl;
+    }
     // * Print all the prime numbers between a and b.
     int a, b;
     cin >> a >> b;
