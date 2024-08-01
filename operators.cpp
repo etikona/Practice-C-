3c6a263a609c477989e28d54e399138d0c8cafc3
eti kona <etikonapal@gmail.com>
2024-08-01 12:34:21 +0600

    Practice Questions solve one

--- operators.cpp
+++ operators.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int i =10, j=20, k;
-        //10   //9  //19    //20 //9   //20 //19 //20
-     k= i-- - i++ + --j - ++j + --i - j-- + ++i - j++;
-     cout << i << endl;
-     cout << j << endl;
-     cout << k << endl;
+    // int i =10, j=20, k;
+   // ! //     //10   //9  //19    //20 //9   //20 //19 //20
+    //  k= i-- - i++ + --j - ++j + --i - j-- + ++i - j++;
+    //  cout << i << endl;
+    //  cout << j << endl;
+    //  cout << k << endl;
 
     return 0;
 }
---
+++ practiceQuestions.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    //* 1) Write a program to print numbers from 1 to 10 using a for loop.
+    // int n = 10;
+    // for(int i=1; i <=10; i++){
+    //     cout << i << endl;
+    // }
+    
+
+    return 0;
+}
